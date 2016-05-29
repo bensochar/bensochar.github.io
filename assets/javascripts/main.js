8aedaf0d98fb016130d583c82f04653018ccd366
ben sochar <bsochar@gmail.com>
2016-05-29 02:12:34 -0400

    updates to project gallery, next and prev nav for pojects colletions, more work examples

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,12 +1,17 @@
+window.onload = function() {
+  init();
+};
 
-// if(document.getElementById('js-gallery-project')){
-//   var elem = document.querySelector('.gallery-project');
-//   var iso = new Isotope(elem, {
-//     // options
-//     itemSelector: '.gallery-project-item',
-//     layoutMode: 'masonry'
-//       // layoutMode: 'masonry',
-//       // percentPosition: true
-//       // layoutMode: 'fitRows'
-//   });
-// }
+init = function() {
+  if(document.getElementById('js-gallery-project')){
+    var elem = document.querySelector('.gallery-project');
+    var iso = new Isotope(elem, {
+      // options
+      itemSelector: '.gallery-project-item',
+      layoutMode: 'masonry'
+        // layoutMode: 'masonry',
+        // percentPosition: true
+        // layoutMode: 'fitRows'
+    });
+  }
+};
